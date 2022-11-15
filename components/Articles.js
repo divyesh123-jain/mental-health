f3bbc9a45c983f685427ad94495937c9608e1c5b
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2022-11-15 21:25:21 +0530

    second commit

---
+++ components/Articles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Articles = () => {
+  return (<>
+    <div className='pt-72'>
+      <h1 className='flex justify-center text-4xl font-bold '><span className=' border-b-8 border-indigo-500 '>ARTICLES</span></h1>
+    </div>
+    </>
+  );
+}
+
+export default Articles;
